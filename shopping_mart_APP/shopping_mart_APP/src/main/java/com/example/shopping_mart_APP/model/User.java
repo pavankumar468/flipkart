209268eb0dc5abb6c502d83c55b25ca1b5904c3d
Pavan Kumar <pavankumarjan8@gmail.com>
2024-03-08 14:18:06 +0530

    create user entity with controller,service,repo,entity packages

---
+++ shopping_mart_APP/shopping_mart_APP/src/main/java/com/example/shopping_mart_APP/model/User.java
@@ -0,0 +1,27 @@
+package com.example.shopping_mart_APP.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long user_id;
+    private String name;
+    private String email;
+}
