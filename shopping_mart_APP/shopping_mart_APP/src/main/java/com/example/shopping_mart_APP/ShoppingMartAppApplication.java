6f2c51b8573d13ee45879348b8ed0e8bca3b972b
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2024-03-08 12:55:37 +0530

    Add files via upload
    1st commit

---
+++ shopping_mart_APP/shopping_mart_APP/src/main/java/com/example/shopping_mart_APP/ShoppingMartAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping_mart_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMartAppApplication.class, args);
+	}
+
+}
---
+++ shopping_mart_APP/shopping_mart_APP/src/test/java/com/example/shopping_mart_APP/ShoppingMartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_mart_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
